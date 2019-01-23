67ecbf8ee089e3ece157a525217f1ae1d8f2f3c0
RsfSingularity7696 <35178708+RsfSingularity7696@users.noreply.github.com>
2019-01-22 20:02:35 -0800

    Updated Vuforia module.
    Updated module to use parameters needed for the webcam.

--- Modules/VuforiaModule.java
+++ Modules/VuforiaModule.java
@@ -7,15 +7,15 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 
 public class VuforiaModule {
-    private static final String VUFORIA_KEY = "AZjwTlP/////AAAAGQ0Nw67MdEzAm0Cuvb+oJGOCSFyqWb6WplQDZyJo9vOTw0iLTPRXEXyjZ/WAN+V4wKPlbTv0pbPad2yBRZ3vyHl4hRafu7wJGLMgzfsTeozU4SzDgl378Q3zGZsJWK+UkfmekmXkzeysQqnkL0rZK+1KNaARZjbR14/OONP2j9AjqOoJa6yMMMN/2jgM5x/Lshv/++vTyHUzJYs7rEHh26FGDtlE9J8FoRXaNPrii2i3j2msG2bTO0AzrlwnK6AMtP92bqRGHnIvU4GQVnMgi2fn/EoxNRLiNktloDfdzv9vRaU3zgpOENxmqzkfdBskZzfy6EYGsEzMQPtJ07brBfDhC0l9w4kvuBi2i5dK7Mbo";
+    private static final String VUFORIA_KEY = "ARHbQtb/////AAABmWcIXrrnZERtjdHTy4MLMsBfrh68ImsBSX4N2+nD+E4HiRVLKmQkN+xrViWcSCKbMYpSRjwM0Ug0IuKFeZ61GHCWEZ/70fh3R6Q1ZLauKTBXPZfoRu64tDEYOUDdIpsU9fd/wpkNq4mRWDyVxfQGjiP2CHefp9pLAFQ+Eesb0HC0Z1l7dsPIJD6je0wEOCKnahQzQE7ZJ1ojBFQYj3eCrnfBPVXgPB9tdPHiMeNnvAvGRg8qFVP+OJEjD08/J3maDAbLdUOZVRKkNTe56EQO/eaFSwLBvwyoRCurHs64EEjUyIRjUBoz1OyJ75vVR7QYrozq9LjwrF+8OlqrfxFpfab5mSR/0PzyZ3vx5/jbjJYl";
 
     protected VuforiaLocalizer vuforia;
 
     public void init(OpMode opMode) {
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
         parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        //parameters.cameraName = opMode.hardwareMap.get(WebcamName.class, "webcam");
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        //parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        parameters.cameraName = opMode.hardwareMap.get(WebcamName.class, "Webcam 1");
 
         vuforia = ClassFactory.getInstance().createVuforia(parameters);
     }
